7fe3384bbd5657f42d054e5ec27df42eb8ba94e1
Mohammad Abubaqr <mohammadabubaqr@outlook.com>
2023-03-09 16:25:37 +0500

    Initial Commit

---
+++ Coding Challenge/Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coding_ChallengeApp.swift
+//  Coding Challenge
+//
+//  Created by Mohammad Abubaqr on 09/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
